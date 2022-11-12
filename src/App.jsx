1c977061172fa83b2974908bfcbe7b11fb07d050
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-11-12 23:22:56 +0100

    -

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import UseSpring from './animations/useSpring/UseSpring';
+import UseSprings from './animations/useSprings/UseSprings';
+import UseTransition from './animations/useTransition/UseTransition';
+import UseTrail from './animations/useTrail/useTrail';
+import UseChain from './animations/useChain/useChain';
+
+function App() {
+  return (
+    <>
+      {/* <UseSpring /> */}
+      {/* <UseSprings /> */}
+      {/* <UseTransition /> */}
+      {/* <UseTrail /> */}
+      {/* <UseChain /> */}
+    </>
+  );
+}
+
+export default App;
