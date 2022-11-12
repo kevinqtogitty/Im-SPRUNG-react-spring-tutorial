8bb52170f1fc3a2d435f36ebffec5475773b96df
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-11-12 10:26:53 +0100

    -

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UseSpring from './animations/useSpring/UseSpring';
+
+function App() {
+  return (
+    <>
+      <UseSpring />
+    </>
+  );
+}
+
+export default App;
