ec9fe44ae08f7f97234e096d42d70bf66f85c7ac
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-11-13 11:05:12 +0100

    -

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import UseSprings from './animations/useSprings/UseSprings';
 import UseTransition from './animations/useTransition/UseTransition';
 import UseTrail from './animations/useTrail/useTrail';
 import UseChain from './animations/useChain/useChain';
+import { MainContainer } from './styles/styles';
 
 function App() {
+  // Uncomment to see examples
   return (
-    <>
-      {/* <UseSpring /> */}
+    <MainContainer>
+      <UseSpring />
       {/* <UseSprings /> */}
       {/* <UseTransition /> */}
       {/* <UseTrail /> */}
       {/* <UseChain /> */}
-    </>
+    </MainContainer>
   );
 }
 
